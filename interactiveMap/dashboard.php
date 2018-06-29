ffab967df359fbb6a49f63858ca3082295a9f7fe
YonaSmilevitch <EstherJudith92>
2018-06-29 12:58:45 +0200

    Merge branch 'master' of APPOE CORE
    # Optimizing

---
+++ interactiveMap/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+require('main.php');
+$InteractiveMap = new App\Plugin\InteractiveMap\InteractiveMap();
+$pagesCount = $InteractiveMap->showAll(true);
+
+if ($pagesCount) {
+    echo json_encode(
+        array(
+            'name' => trans('Cartes Interactives'),
+            'count' => $pagesCount,
+            'url' => WEB_PLUGIN_URL . 'interactiveMap/page/allInterMaps/'
+        ), JSON_UNESCAPED_UNICODE
+    );
+}
