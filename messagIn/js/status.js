1365619f35a8837b3f8ce849d77b3125b93b8320
YonaSmilevitch <EstherJudith92>
2018-07-06 12:14:19 +0200

    Merge branch 'master' of APPOE
    # Optimizing

--- messagIn/js/status.js
+++ messagIn/js/status.js
@@ -7,13 +7,21 @@ function liveMessages() {
 
 jQuery(document).ready(function () {
 
-    liveMessages();
+    liveMessages().done(function (data) {
+        if ($.isNumeric(data) || data == 0) {
+            if (!$('#menu-messages > a span.badge').length) {
+                $('#menu-messages > a').append(' <span class="badge badge-pill badge-danger ml-1">' + data + '</span>');
+            } else {
+                $('#menu-messages > a span.badge').html(data);
+            }
+        }
+    });
 
     //Start Cron
     var msgCron = setInterval(function () {
 
         liveMessages().done(function (data) {
-            if ($.isNumeric(data)) {
+            if ($.isNumeric(data) || data == 0) {
                 if (!$('#menu-messages > a span.badge').length) {
                     $('#menu-messages > a').append(' <span class="badge badge-pill badge-danger ml-1">' + data + '</span>');
                 } else {
