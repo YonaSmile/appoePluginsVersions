22ac0dbe77b509517f171f96c3fda42ec9e7420e
YonaSmilevitch <EstherJudith92>
2018-10-11 16:36:26 +0200

    Merge branch 'master' of APPOE
    # Optimizing

--- cms/include/cms_functions.php
+++ cms/include/cms_functions.php
@@ -27,3 +27,63 @@ function loadPage($slug = 'home')
 
     return $pageContent;
 }
+
+/**
+ * @param bool $otherParentSlug
+ * @return array
+ */
+function breadcrumb($otherParentSlug = false)
+{
+    $Cms = new \App\Plugin\Cms\Cms();
+
+    $currentUrl = $_SERVER['REQUEST_URI'];
+    $currentUrlSlug = basename($currentUrl);
+    $currentName = '';
+    $currentSlug = '';
+
+    $parentUrlSlug = basename(dirname($currentUrl));
+    $parentName = '';
+    $parentSlug = '';
+
+    //Get Home Data
+    $Cms->setSlug('home');
+    $Cms->showBySlug();
+    $homeName = $Cms->getName();
+    $homeSlug = webUrl('home/');
+
+    //Get Parent Data
+    if (!empty($parentUrlSlug) && $parentUrlSlug != '/' && $parentUrlSlug != '.') {
+
+        if (false !== $otherParentSlug) {
+            $Cms->setSlug($otherParentSlug);
+        } else {
+            $Cms->setSlug($parentUrlSlug);
+        }
+
+        $Cms->showBySlug();
+
+        $parentName = $Cms->getName();
+        $parentSlug = webUrl($Cms->getSlug() . '/');
+    }
+
+    //Get Current Data
+    $Cms->setSlug($currentUrlSlug);
+
+    if ($Cms->showBySlug()) {
+        $currentName = $Cms->getName();
+        $currentSlug = webUrl($Cms->getSlug() . '/');
+    }
+
+    //Response Data
+    $breadcrumb = array(
+        'homeUrl' => $homeSlug,
+        'homeSlug' => $homeName,
+        'parentUrl' => $parentSlug,
+        'parentSlug' => $parentName,
+        'currentUrl' => $currentSlug,
+        'currentSlug' => $currentName
+    );
+
+    return $breadcrumb;
+
+}
