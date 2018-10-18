d16c0b0ee3963f18b3b8fde696c62d1b792a04f1
YonaSmilevitch <EstherJudith92>
2018-10-18 15:36:43 +0200

    Merge branch 'master' of APPOE
    # Optimizing

--- shop/dashboard.php
+++ shop/dashboard.php
@@ -6,16 +6,27 @@ $allCancelCommandes = $Commande->showAll(0, true, true, 1);
 $allLiveCommandes = $Commande->showAll(0, true, true, 2);
 $allConfirmCommandes = $Commande->showAll(0, true, true, 3);
 
+$Product = new \App\Plugin\Shop\Product();
+$allProducts = $Product->showAll(true);
+
 $Menu = new \App\Menu();
-$menuData = $Menu->displayMenuBySlug('commandes');
+$menuCommandData = $Menu->displayMenuBySlug('commandes');
+$menuProductData = $Menu->displayMenuBySlug('products');
 
 if (false !== $allCommandes) {
     echo json_encode(
         array(
-            'name' => trans($menuData->name),
-            'count' => $allCommandes,
-            'url' => WEB_PLUGIN_URL . 'shop/page/commandes/',
-            'html' => '<span>Confirmées: ' . $allConfirmCommandes . '</span><span>En cours: ' . $allLiveCommandes . '</span><span>Annulées: ' . $allCancelCommandes . '</span>'
+            1 => array(
+                'name' => trans($menuCommandData->name),
+                'count' => $allCommandes,
+                'url' => WEB_PLUGIN_URL . 'shop/page/commandes/',
+                'html' => '<span>Confirmées: ' . $allConfirmCommandes . '</span><span>En cours: ' . $allLiveCommandes . '</span><span>Annulées: ' . $allCancelCommandes . '</span>'
+            ),
+            2 => array(
+                'name' => trans($menuProductData->name),
+                'count' => $allProducts,
+                'url' => WEB_PLUGIN_URL . 'shop/page/products/'
+            )
         ), JSON_UNESCAPED_UNICODE
     );
 }
