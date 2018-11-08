593e6a0202eebb6e58887f0bd8ee64ed1c4a2f16
YonaSmilevitch <EstherJudith92>
2018-11-08 12:49:39 +0100

    Merge branch 'master' of APPOE
    # Optimizing

--- agapesHotes/page/updateMainCourante.php
+++ agapesHotes/page/updateMainCourante.php
@@ -37,7 +37,7 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
         ?>
         <div class="container-fluid">
             <div class="table-responsive col-12">
-                <table id="pagesTable" class="table table-striped">
+                <table class="table table-striped tableNonEffect">
                     <thead>
                     <tr>
                         <th><?= trans('Prestation'); ?></th>
@@ -50,7 +50,10 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
                     <?php foreach ($allPrestations as $prestation):
                         if (array_key_exists($prestation->id, $allMainCourante)): ?>
                             <tr data-idprestation="<?= $prestation->id ?>">
-                                <th><?= $prestation->nom; ?></th>
+                                <th class="positionRelative" style="vertical-align: middle;"><?= $prestation->nom; ?>
+                                    <small class="prestationReelPrice"
+                                           style="position: absolute;top: 0; right: 3px;font-size: 0.7em;"></small>
+                                </th>
                                 <?php foreach ($period as $key => $date):
 
                                     $allPrestationsPrixDates = array_keys($allPrestationsPrix[$prestation->id]);
@@ -79,24 +82,53 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
                                     }
 
                                     ?>
+
                                     <td style="padding: 4px !important;" data-toggle="tooltip" data-placement="right"
+                                        data-prixprestation="<?= $prixReel; ?>"
                                         title="<?= $date->format('d') . ' / ' . $prestation->nom; ?>">
                                         <input type="tel" data-prestationid="<?= $prestation->id; ?>"
                                                data-date="<?= $date->format('Y-m-d'); ?>"
                                                data-prixid="<?= $idPrixReel; ?>"
+                                               data-day="<?= $date->format('d'); ?>"
                                                class="text-center form-control mainCourantInput"
                                                name="<?= $mainCourantId; ?>"
                                                value="<?= $mainCourantQuantite; ?>"
                                                style="padding: 5px 0 !important; <?= $date->format('d') == date('d') ? 'background:#4fb99f;color:#fff;' : ''; ?>">
+                                        <small class="d-block text-center prestationPrice"
+                                               data-day="<?= $date->format('j'); ?>"
+                                               data-prixprestation="<?= $prixReel; ?>"><?= financial($prixReel * $mainCourantQuantite); ?>
+                                        </small>
                                     </td>
                                 <?php endforeach; ?>
                             </tr>
                         <?php endif;
                     endforeach; ?>
+                    <tr>
+                        <th><?= trans('Total'); ?></th>
+                        <?php foreach ($period as $key => $date): ?>
+                            <td style="<?= $date->format('d') == date('d') ? 'background:#4fb99f;color:#fff !important;' : ''; ?> <?= $date->format('N') == 7 ? 'background:#f2b134;color:#4b5b68;' : ''; ?> font-size: 0.7em !important;"
+                                class="totalDayPrestationPrice" data-day="<?= $date->format('j'); ?>"></td>
+                        <?php endforeach; ?>
+                    </tr>
+                    </tbody>
+                </table>
+                <table class="table table-striped tableNonEffect" style="width: 200px;">
+                    <tbody>
+                    <tr>
+                        <th>CA Mois</th>
+                        <td class="caPrestationMonth"></td>
+                    </tr>
+                    <tr>
+                        <th>CA Semaine</th>
+                        <td class="caPrestationWeek"></td>
+                    </tr>
+                    <tr>
+                        <th>CA Jour</th>
+                        <td class="caPrestationDay"></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
-
         </div>
         <script>
             $(document).ready(function () {
@@ -108,6 +140,73 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
                 function activateAllFields() {
                     $('input.mainCourantInput').attr('disabled', false);
                 }
+
+                function calculateTotalPrestationPricePerDay() {
+                    var totalPriceByDay = [];
+                    $('.prestationPrice').each(function () {
+                        var $info = $(this);
+                        var day = $info.data('day');
+                        var totalDay = parseFloat($info.text());
+
+                        if (typeof totalPriceByDay[day] === 'undefined') {
+                            totalPriceByDay[day] = [];
+                        }
+                        totalPriceByDay[day].push(totalDay);
+                    });
+
+                    $.each(totalPriceByDay, function (key, value) {
+                        var sum = 0;
+                        $.each(value, function (i, val) {
+                            sum += val;
+                        });
+                        $('.totalDayPrestationPrice[data-day="' + key + '"]').html(financial(sum));
+                    });
+                    calculateAveragePrestationPricePerMonth();
+                }
+
+                function calculateAveragePrestationPricePerMonth() {
+                    var totalPriceByDay = [];
+                    $('.totalDayPrestationPrice').each(function () {
+                        var $totalDay = $(this);
+                        var totalDay = parseFloat($totalDay.text());
+
+                        totalPriceByDay.push(totalDay);
+                    });
+                    var sum = 0;
+                    $.each(totalPriceByDay, function (key, value) {
+                        sum += value;
+                    });
+                    $('.caPrestationMonth').html(financial(sum)+'€');
+
+                    calculateAveragePrestationPricePerDay();
+                }
+
+                function calculateAveragePrestationPricePerDay() {
+
+                    var totalMonth = parseFloat($('.caPrestationMonth').text());
+                    var averageDay = (totalMonth / parseFloat(<?= date('t'); ?>));
+
+                    $('.caPrestationDay').html(financial(averageDay)+'€');
+
+                    calculateAveragePrestationPricePerWeek();
+                }
+
+                function calculateAveragePrestationPricePerWeek() {
+
+                    var totalDays = parseFloat($('.caPrestationDay').text());
+                    var averageWeek = (totalDays * 7);
+
+                    $('.caPrestationWeek').html(financial(averageWeek)+'€');
+                }
+
+
+                $('tr[data-idprestation!=""]').each(function () {
+                    var $tr = $(this);
+                    var prestationPrice = $('td', $tr).eq(1).data('prixprestation');
+                    $tr.find('.prestationReelPrice').html(prestationPrice + '€');
+                });
+
+                calculateTotalPrestationPricePerDay();
 
                 var delay = (function () {
                     var timer = 0;
@@ -123,6 +222,8 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
                     event.preventDefault();
 
                     var $Input = $(this);
+                    var $InputInfo = $Input.next('small.prestationPrice');
+
                     $('input.mainCourantInput').removeClass('successInput');
 
                     if (!$Input.val().length > 0) {
@@ -160,6 +261,8 @@ if (!empty($_GET['secteur']) && !empty($_GET['site'])):
                                         alert(data);
                                     }
                                     availableApp();
+                                    var prestationPrice = parseFloat($InputInfo.data('prixprestation'));
+                                    $InputInfo.html(financial(quantite * prestationPrice));
                                     activateAllFields();
                                 }
                             );
