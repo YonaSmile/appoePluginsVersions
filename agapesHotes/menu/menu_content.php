360243675643fcd558dda1cae21d3901276e80a2
YonaSmilevitch <EstherJudith92>
2018-12-06 12:20:05 +0100

    Merge branch 'master' of APPOE
    # Optimizing

--- agapesHotes/menu/menu_content.php
+++ agapesHotes/menu/menu_content.php
@@ -4,16 +4,24 @@ if (getUserRoleId() == 1):
     $SiteAccess->setSiteUserId(getUserIdSession());
     $Site = $SiteAccess->showSiteByUser();
     ?>
-    <li class="<?= activePage('mainCourante'); ?>">
-        <a href="<?= AGAPESHOTES_URL; ?>page/mainCourante/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
+    <li class="<?= activePage('updateMainCourante'); ?>">
+        <a href="<?= WEB_PLUGIN_URL; ?>agapesHotes/page/mainCourante/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
             <?= trans('Main Courante'); ?></a>
-        <a href="<?= AGAPESHOTES_URL; ?>page/planning/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
+    </li>
+    <li class="<?= activePage('updatePlanning'); ?>">
+        <a href="<?= WEB_PLUGIN_URL; ?>agapesHotes/page/planning/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
             <?= trans('Planning'); ?></a>
-        <a href="<?= AGAPESHOTES_URL; ?>page/noteDeFrais/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
+    </li>
+    <li class="<?= activePage('updateNoteDeFrais'); ?>">
+        <a href="<?= WEB_PLUGIN_URL; ?>agapesHotes/page/noteDeFrais/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
             <?= trans('Note de frais'); ?></a>
-        <a href="<?= AGAPESHOTES_URL; ?>page/vivreCrue/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
+    </li>
+    <li class="<?= activePage('updateVivreCrue'); ?>">
+        <a href="<?= WEB_PLUGIN_URL; ?>agapesHotes/page/vivreCrue/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
             <?= trans('Vivre crue'); ?></a>
-        <a href="<?= AGAPESHOTES_URL; ?>page/mainSupplementaire/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
+    </li>
+    <li class="<?= activePage('updateMainSupplementaire'); ?>">
+        <a href="<?= WEB_PLUGIN_URL; ?>agapesHotes/page/mainSupplementaire/<?= $Site->secteurSlug; ?>/<?= $Site->slug; ?>/">
             <?= trans('Facturation HC'); ?></a>
     </li>
 <?php endif; ?>
