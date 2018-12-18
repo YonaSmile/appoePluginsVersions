91517486f7c99a4285cd5fcb1ebee2a2db466993
YonaSmilevitch <EstherJudith92>
2018-12-18 15:27:49 +0100

    Merge branch 'master' of APPOE
    # Optimizing

--- agapesHotes/js/footer.js
+++ agapesHotes/js/footer.js
@@ -1,26 +1,23 @@
-$(document).ready(function () {
+if ($('.fixed-header').length) {
+    var tablePosition = $('.fixed-header').offset();
+    var theadSize = {};
 
-    if($('.fixed-header').length) {
-        var tablePosition = $('.fixed-header').offset();
-        var theadSize = {};
-
-        $(window).scroll(function (e) {
-            $('.fixed-header thead th').each(function (index, val) {
-                theadSize[index] = $(this).width();
-            });
-            var top = this.scrollY, left = this.scrollX;
-            if (top >= parseInt(tablePosition.top)) {
-                $('.fixed-header thead').stop().css({
-                    top: (top - parseInt(tablePosition.top)) + $('#navbarUser').outerHeight() - 10,
-                    left: 0,
-                    position: 'absolute'
-                });
-                $('.fixed-header thead th').each(function (index, val) {
-                    $(this).width(theadSize[index]);
-                });
-            } else {
-                $('.fixed-header thead').css({top: 0, left: 0, position: 'static'});
-            }
+    $(window).scroll(function (e) {
+        $('.fixed-header thead th').each(function (index, val) {
+            theadSize[index] = $(this).width();
         });
-    }
-});
+        var top = this.scrollY, left = this.scrollX;
+        if (top >= parseInt(tablePosition.top)) {
+            $('.fixed-header thead').stop().css({
+                top: (top - parseInt(tablePosition.top)) + $('#navbarUser').outerHeight() - 10,
+                left: 0,
+                position: 'absolute'
+            });
+            $('.fixed-header thead th').each(function (index, val) {
+                $(this).width(theadSize[index]);
+            });
+        } else {
+            $('.fixed-header thead').css({top: 0, left: 0, position: 'static'});
+        }
+    });
+}
