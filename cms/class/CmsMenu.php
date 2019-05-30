d834ff483689fe678573e0e48fa47c4965621f05
YonaSmilevitch <EstherJudith92>
2019-05-30 09:01:30 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- cms/class/CmsMenu.php
+++ cms/class/CmsMenu.php
@@ -209,7 +209,7 @@ class CmsMenu
         FROM appoe_plugin_cms_menu AS acm 
         LEFT JOIN appoe_plugin_cms AS ac 
         ON (acm.idCms = ac.id) 
-        WHERE (ac.statut = 1 OR acm.idCms like "http%" OR acm.idCms REGEXP "^[a-zA-Z0-9/-]+$")' . $locationCondition . ' 
+        WHERE (ac.statut = 1 OR acm.idCms like "http%" OR acm.idCms like "%#%" OR acm.idCms REGEXP "^[a-zA-Z0-9/-]+$")' . $locationCondition . ' 
         ORDER BY acm.parentId ASC, acm.position ASC';
         $stmt = $this->dbh->prepare($sql);
         $stmt->execute();
