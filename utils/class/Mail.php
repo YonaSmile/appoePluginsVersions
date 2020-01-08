4a550f8636ad07b4e95f90e0bfc6bde79ec6e224
YonaSmilevitch <EstherJudith92>
2020-01-08 10:45:09 +0100

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- utils/class/Mail.php
+++ utils/class/Mail.php
@@ -12,11 +12,6 @@ class Mail
         if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])
             && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 
-            //User Stay Connected
-            if ($connectedStatus && function_exists('mehoubarim_connecteUser')) {
-                mehoubarim_connecteUser();
-            }
-
             //Clean Post
             $recaptchaField = cleanRequest($post['g-recaptcha-response']);
 
