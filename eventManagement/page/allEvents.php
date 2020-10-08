87a5fdaadb911e56d558ed599dd603d9e1ca5aaf
YonaSmilevitch <EstherJudith92>
2020-10-08 13:23:17 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- eventManagement/page/allEvents.php
+++ eventManagement/page/allEvents.php
@@ -1,5 +1,5 @@
 <?php require('header.php');
-echo getTitle( getAppPageName(), getAppPageSlug() ); ?>
+echo getTitle(getAppPageName(), getAppPageSlug()); ?>
     <div class="container-fluid">
         <?php
         $Event = new \App\Plugin\EventManagement\Event();
@@ -22,34 +22,37 @@ echo getTitle( getAppPageName(), getAppPageSlug() ); ?>
                         </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($evenements as $evenement): ?>
-                            <?php $Auteur->setId($evenement->auteurId);
-                            $Auteur->show(); ?>
-                            <tr>
-                                <td><?= $Auteur->getName(); ?></td>
-                                <td><?= $evenement->titre; ?></td>
-                                <td><?= displayDuree($evenement->duree) ?></td>
-                                <td><?= trans(SPECTACLES_TYPES[$evenement->spectacleType]); ?></td>
-                                <td><?= trans(INDOOR_OFF[$evenement->indoor]); ?></td>
-                                <td>
-                                    <a href="<?= getPluginUrl('eventManagement/page/event/', $evenement->id) ?>"
-                                       class="btn btn-sm"
-                                       title="<?= trans('Consulter'); ?>">
-                                        <span class="btnUpdate"><i class="fas fa-cog"></i></span>
-                                    </a>
-                                    <a href="<?= getPluginUrl('eventManagement/page/event/update/', $evenement->id) ?>"
-                                       class="btn btn-sm"
-                                       title="<?= trans('Modifier'); ?>">
-                                        <span class="btnEdit"><i class="fas fa-wrench"></i></span>
-                                    </a>
-                                    <button type="button" class="btn btn-sm deleteEvent"
-                                            title="<?= trans('Archiver'); ?>"
-                                            data-idevent="<?= $evenement->id ?>">
-                                        <span class="btnArchive"><i class="fas fa-archive"></i></span>
-                                    </button>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
+                        <?php
+                        if ($evenements):
+                            foreach ($evenements as $evenement):
+                                $Auteur->setId($evenement->auteurId);
+                                $Auteur->show(); ?>
+                                <tr>
+                                    <td><?= $Auteur->getName(); ?></td>
+                                    <td><?= $evenement->titre; ?></td>
+                                    <td><?= displayDuree($evenement->duree) ?></td>
+                                    <td><?= trans(SPECTACLES_TYPES[$evenement->spectacleType]); ?></td>
+                                    <td><?= trans(INDOOR_OFF[$evenement->indoor]); ?></td>
+                                    <td>
+                                        <a href="<?= getPluginUrl('eventManagement/page/event/', $evenement->id) ?>"
+                                           class="btn btn-sm"
+                                           title="<?= trans('Consulter'); ?>">
+                                            <span class="btnUpdate"><i class="fas fa-cog"></i></span>
+                                        </a>
+                                        <a href="<?= getPluginUrl('eventManagement/page/event/update/', $evenement->id) ?>"
+                                           class="btn btn-sm"
+                                           title="<?= trans('Modifier'); ?>">
+                                            <span class="btnEdit"><i class="fas fa-wrench"></i></span>
+                                        </a>
+                                        <button type="button" class="btn btn-sm deleteEvent"
+                                                title="<?= trans('Archiver'); ?>"
+                                                data-idevent="<?= $evenement->id ?>">
+                                            <span class="btnArchive"><i class="fas fa-archive"></i></span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php endforeach;
+                        endif; ?>
                         </tbody>
                     </table>
                 </div>
