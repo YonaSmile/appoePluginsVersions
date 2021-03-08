9d0125ca4f75d8d54092df45c487cb70fdb5ca2f
YonaSmilevitch <EstherJudith92>
2021-03-08 13:03:37 +0100

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- instagram/js/instagram_base.js
+++ instagram/js/instagram_base.js
@@ -2,20 +2,30 @@ function getInstagramTimelineFile() {
     return $.getJSON('/app/plugin/instagram/timeline.json');
 }
 
-function getInstagramTimeline(preferences) {
-    let timeline = [];
-    let options = {
-        count: 5
-    }
-    $.extend(options, preferences);
+function showInstagramTimeline(preferences = {}) {
 
-    getInstagramTimelineFile().done(function(data){
-        let instaJson = data.graphql.user.edge_owner_to_timeline_media.edges;
-        for (let c = 0; c < options.count; c++) {
-            let obj = instaJson[c].node;
-            obj.link = data.link + obj.shortcode + '/';
-            timeline.push(obj);
+    let options = {
+        count: 5,
+        container: '#instagramTimelineContainer',
+        thumbnail: false
+    }
+    let obj;
+    let $timelineContainer = $(options.container);
+    $.merge(options, preferences);
+
+    getInstagramTimelineFile().done(function (timeline) {
+        if (timeline) {
+            for (let c = 0; c < options.count; c++) {
+                if (timeline.data[c]) {
+                    obj = timeline.data[c];
+                    if (obj.media_type !== 'VIDEO') {
+                        let imgUrl = (options.thumbnail && obj.thumbnail_url) ? obj.thumbnail_url : obj.media_url;
+                        let img = '<img src="' + imgUrl + '" alt="' + obj.caption + '">';
+                        let item = '<a href="' + obj.permalink + '" target="_blank">' + img + '</a>';
+                        $timelineContainer.append(item);
+                    }
+                }
+            }
         }
-        return timeline;
     });
 }
--- instagram/perso_dashboard.php
+++ instagram/perso_dashboard.php
@@ -6,13 +6,12 @@ if (defined('INSTAGRAM_USERNAME') && !empty(INSTAGRAM_USERNAME)):
         <div class="d-flex col-12 col-lg-8">
             <div class="card border-0 w-100">
                 <div class="card-header bg-white pb-0 border-0 boardBlock1Title">
-                    <h5 class="m-0 pl-4 colorPrimary">Instagram <?= trans('Timeline'); ?> <span class="badge bgColorPrimary"><?= INSTAGRAM_USERNAME; ?></span></h5>
+                    <h5 class="m-0 pl-4 colorPrimary">Instagram <?= trans('Timeline'); ?> <span
+                                class="badge bgColorPrimary"><?= INSTAGRAM_USERNAME; ?></span></h5>
                     <hr class="mx-4">
                 </div>
-                <div class="card-body pt-0" id="instagramTimelineContenair">
-                    <button type="button" id="updateTimeline" class="btn btn-sm colorPrimary bgColorPrimary">
-                        Mettre à jour
-                    </button>
+                <div class="card-body pt-0" id="instagramTimelineContainer">
+
                 </div>
             </div>
         </div>
@@ -22,16 +21,23 @@ if (defined('INSTAGRAM_USERNAME') && !empty(INSTAGRAM_USERNAME)):
                     <h5 class="m-0 pl-4 colorPrimary"><?= trans('Dernière mise à jour'); ?></h5>
                     <hr class="mx-4">
                 </div>
-                <div class="card-body pt-0" id="instagramTimelineInfos">
-                    <p><?= file_exists(TIMELINE_FILE)
+                <div class="card-body pt-0 d-flex justify-content-between"
+                     id="instagramTimelineInfos">
+                    <p class="align-self-start"><?= file_exists(TIMELINE_FILE)
                             ? displayCompleteDate(getJsonContent(TIMELINE_FILE, 'lastUpdate'), true)
                             : trans('Le fichier de la timeline n\'existe pas'); ?></p>
+                    <button type="button" id="updateTimeline" class="btn btn-sm colorPrimary bgColorPrimary align-self-end">
+                        Mettre à jour
+                    </button>
                 </div>
             </div>
         </div>
     </div>
     <script>
         $(document).ready(function () {
+
+            let $timelineContainer = $('#instagramTimelineContainer');
+
             $('#updateTimeline').on('click', function (event) {
                 event.preventDefault();
 
@@ -40,34 +46,33 @@ if (defined('INSTAGRAM_USERNAME') && !empty(INSTAGRAM_USERNAME)):
                 busyApp();
 
                 let username = $('#instagramContainer').data('instagram-username');
-                let instaDataUrl = 'https://www.instagram.com/'+username+'/?__a=1';
 
-                $.get(instaDataUrl).done(function (data) {
-                    $.post('/app/plugin/instagram/process/ajaxProcess.php', {updateTimeline: data}).done(function (data) {
-                        if (data == 'true' || data === true) {
+                $.post('/app/plugin/instagram/process/ajaxProcess.php', {updateTimeline: 'OK'}).done(function (data) {
+                    if (data == 'true' || data === true) {
 
-                            $button.remove();
-                            $('#instagramTimelineContenair').html('<p>La timeline de <strong>' + username + '</strong> a été mise à jour !</p>');
+                        $button.remove();
+                        $timelineContainer.html('<p>La timeline de <strong>' + username + '</strong> a été mise à jour !</p>');
 
-                            let d = new Date();
+                        let d = new Date();
 
-                            let month = d.getMonth() + 1;
-                            let day = d.getDate();
+                        let month = d.getMonth() + 1;
+                        let day = d.getDate();
 
-                            let output = (day < 10 ? '0' : '') + day
-                                + '/' + (month < 10 ? '0' : '') + month
-                                + '/' + d.getFullYear()
-                                + ' ' + d.getHours() + ':' + d.getMinutes();
+                        let output = (day < 10 ? '0' : '') + day
+                            + '/' + (month < 10 ? '0' : '') + month
+                            + '/' + d.getFullYear()
+                            + ' ' + d.getHours() + ':' + d.getMinutes();
 
-                            $('#instagramTimelineInfos').html('<p>' + output + '</p>');
+                        $('#instagramTimelineInfos').html('<p>' + output + '</p>');
 
-                        } else {
-                            $('#instagramTimelineContenair').html('<p>Une erreur est survenue lors de la mise à jour !</p>');
-                        }
-                        availableApp();
-                    });
+                    } else {
+                        $timelineContainer.html('<p>Une erreur est survenue lors de la mise à jour !</p>');
+                    }
+                    availableApp();
                 });
-            })
+
+            });
+            showInstagramTimeline({container: '#instagramTimelineContainer', thumbnail: true});
         });
     </script>
 <?php endif; ?>
