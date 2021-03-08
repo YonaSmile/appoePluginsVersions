01280f74e7539dcf61c4bdf00d87c8f7ceac8cab
YonaSmilevitch <EstherJudith92>
2021-03-08 14:54:48 +0100

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- instagram/perso_dashboard.php
+++ instagram/perso_dashboard.php
@@ -18,17 +18,19 @@ if (defined('INSTAGRAM_USERNAME') && !empty(INSTAGRAM_USERNAME)):
         <div class="d-flex col-12 col-lg-4">
             <div class="card border-0 w-100">
                 <div class="card-header bg-white pb-0 border-0 boardBlock1Title">
-                    <h5 class="m-0 pl-4 colorPrimary"><?= trans('Dernière mise à jour'); ?></h5>
+                    <h5 class="m-0 pl-4 colorPrimary"><?= trans('Dernière mise à jour'); ?>
+                        <button type="button" id="updateTimeline" class="btn btn-sm colorPrimary bgColorPrimary float-right">
+                            Mettre à jour
+                        </button>
+                    </h5>
                     <hr class="mx-4">
                 </div>
-                <div class="card-body pt-0 d-flex justify-content-between"
+                <div class="card-body pt-0"
                      id="instagramTimelineInfos">
-                    <p class="align-self-start"><?= file_exists(TIMELINE_FILE)
+                    <p><?= file_exists(TIMELINE_FILE)
                             ? displayCompleteDate(getJsonContent(TIMELINE_FILE, 'lastUpdate'), true)
                             : trans('Le fichier de la timeline n\'existe pas'); ?></p>
-                    <button type="button" id="updateTimeline" class="btn btn-sm colorPrimary bgColorPrimary align-self-end">
-                        Mettre à jour
-                    </button>
+
                 </div>
             </div>
         </div>
@@ -52,6 +54,10 @@ if (defined('INSTAGRAM_USERNAME') && !empty(INSTAGRAM_USERNAME)):
 
                         $button.remove();
                         $timelineContainer.html('<p>La timeline de <strong>' + username + '</strong> a été mise à jour !</p>');
+                        setTimeout(function (){
+                            $timelineContainer.html('');
+                            showInstagramTimeline({container: '#instagramTimelineContainer', thumbnail: true});
+                        }, 1000);
 
                         let d = new Date();
 
