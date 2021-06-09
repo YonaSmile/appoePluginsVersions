2989a22be0a5e6011963eff60c4ff1a4f8836fe9
YonaSmilevitch <EstherJudith92>
2021-06-09 12:49:31 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- tracker/tracker_dashboard.php
+++ tracker/tracker_dashboard.php
@@ -12,10 +12,10 @@ if ($trackerData): ?>
         <div class="w-100">
             <strong class="d-block w-100">
                 <span class="colorSecondary"><i class="fas fa-clock"></i></span> <?= trans('Depuis'); ?>
-                <div class="d-inline mx-1"><input type="date" id="dateStart" class="noBorder" max="<?= date('Y-m-d'); ?>" value="<?= $dateStart; ?>"></div>
-                Au <div class="d-inline mx-1"><input type="date" id="dateEnd" class="noBorder" max="<?= date('Y-m-d'); ?>" value="<?= $dateEnd; ?>"></div>
+                <div class="d-inline-block mx-1"><input type="date" id="dateStart" class="noBorder" max="<?= date('Y-m-d'); ?>" value="<?= $dateStart; ?>"></div>
+                <div class="d-inline-block mx-1"><?= trans('Jusqu\'à'); ?> <input type="date" id="dateEnd" class="noBorder" max="<?= date('Y-m-d'); ?>" value="<?= $dateEnd; ?>"></div>
             </strong>
-            <small class="text-muted" style="display: block;margin: 0 0 0 18px;">Cliquez sur l'icone <i class="fas fa-sync-alt fa-sm mx-1"></i> pour appliquer les changements</small>
+            <small class="text-muted d-block">Cliquez sur l'icone <i class="fas fa-sync-alt fa-sm mx-1"></i> pour appliquer les changements</small>
             <div class="my-4">
                 <div class="my-2 ml-0 ml-lg-4 position-relative">
                     <span class="mr-2"><?= trans('Visiteurs'); ?></span>
