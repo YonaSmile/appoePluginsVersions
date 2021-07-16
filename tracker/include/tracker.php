1cf256aa7adbe9901cd85c600c966b4e755378dc
YonaSmilevitch <EstherJudith92>
2021-07-16 10:29:36 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- tracker/include/tracker.php
+++ tracker/include/tracker.php
@@ -5,7 +5,7 @@ use App\Plugin\Tracker\Tracker;
 require_once($_SERVER['DOCUMENT_ROOT'] . '/app/main.php');
 
 \App\Hook::add_action('core_front_before_html', function () {
-    if (!bot_detected()) {
+    if (!bot_detected() && !isIpAdministrator()) {
         new Tracker(true);
     }
 });
